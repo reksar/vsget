1e337441f2ae8cb4b6435091413d064a76369678
reksarka <reksarka@gmail.com>
2022-08-12 13:18:32 +0300

    Improves uncacher

--- uncache/uncache.bat
+++ uncache/uncache.bat
@@ -1,10 +1,10 @@
 @echo off
 
-rem  Using:
-rem    uncache [VS cache path] [packages group] [package names ...]
-
-rem  Creates %root%\%group%.%version%.bat downloader for given %packages%
-rem  based on Visual Studio cache from given %cache_path%.
+rem  -------------------------------------------------------------------------
+rem  Using: `uncache [PACKAGES GROUP] [PACKAGES PATH PATTERNS ...]`.
+rem  Creates a `%root%\%group%.%version%.bat` downloader for given packages
+rem  %path_patterns% in the Visual Studio cache. A group name can be arbitrary.
+rem  -------------------------------------------------------------------------
 
 setlocal EnableDelayedExpansion
 set root=%~dp0..
@@ -13,20 +13,6 @@ set PATH=%PATH%;%root%\utils
 
 rem  --- Parse args ----------------------------------------------------------
 
-set cache_path=%~1
-
-if "%cache_path%"=="" (
-  echo [ERR] No Visual Studio cache given.
-  goto :END
-)
-
-if not exist "%cache_path%" (
-  echo [ERR] Invalid Visual Studio cache path.
-  goto :END
-)
-
-shift
-
 set group=%~1
 
 if "%group%"=="" (
@@ -34,68 +20,51 @@ if "%group%"=="" (
   goto :END
 )
 
-rem  All remaining args are VS package names.
-set packages=
-:PACKAGES
-shift
-if not "%1"=="" (
-  set packages=%packages% %1
-  goto :PACKAGES
-)
-
-if "%packages%"=="" (
-  echo [ERR] No Visual Studio packages given.
-  goto :END
-)
-
-
-echo Visual Studio cache: %cache_path%
 echo Creating downloader for %group%:
-for %%i in (%packages%) do (
-  echo   %%i
-)
 
+rem  All remaining args are path patterns for VS packages cache dirs.
+rem  NOTE: using an array instead of a list for the %path_patterns% and %dirs%
+rem  allows iterating over dir names containing separators, e.g. spaces and
+rem  commas.
 
-rem  -------------------------------------------------------------------------
-
-echo|set/p=Building search patterns from package names ... 
-
-set /a package_count=0
+set /a pattern_count=0
 set path_patterns=
-
-for %%i in (%packages%) do (
-  set /a package_count+=1
-  set path_patterns=!path_patterns! %cache_path%\Microsoft.VC.*.%%i*
+:PATTERNS
+shift
+if not "%~1"=="" (
+  set /a pattern_count+=1
+  set path_patterns[!pattern_count!]=%~1
+  goto :PATTERNS
 )
 
-if %package_count% EQU 0 (
-  echo.
-  echo   [ERR] Can't count given Visual Studio package names.
+if %pattern_count% EQU 0 (
+  echo [ERR] Can't count given Visual Studio packages path patterns.
   goto :END
 )
 
-echo OK
+for /l %%i in (1,1,%pattern_count%) do (
+  echo   !path_patterns[%%i]!
+)
 
 
 rem  -------------------------------------------------------------------------
 
-echo|set/p=Search for cached Visual Studio %group% packages ... 
-
-rem  NOTE: using array instead of list for the %dirs% allows iterating over
-rem  dir names with string separators, e.g. commas.
+echo|set/p=Searching for cached Visual Studio %group% packages ... 
 
 set /a dir_count=0
 set dirs=
 
-for /d %%i in (%path_patterns%) do (
-  set /a dir_count+=1
-  set dirs[!dir_count!]=%%i
+for /l %%i in (1,1,%pattern_count%) do (
+  for /d %%j in ("!path_patterns[%%i]!") do (
+    set /a dir_count+=1
+    set dirs[!dir_count!]=%%j
+  )
 )
 
-if %dir_count% NEQ %package_count% (
+if %dir_count% NEQ %pattern_count% (
   echo.
-  echo   [ERR] Inconsistent count of package names and related found dirs.
-  echo   Probably found several versions.
+  echo   [ERR] Inconsistent count of package patterns and matched dirs.
+  echo   Probably some packages were not found or several versions were found.
   goto :END
 )
 
@@ -106,7 +75,7 @@ rem  -------------------------------------------------------------------------
 
 echo|set/p=Reading first found package version ... 
 
-set VERSION_PATTERN=\d\d\.\d\d\.\d\d\.\d
+set VERSION_PATTERN=\d+\.\d+\.\d+.\d*
 set get_version=call strmatch "%VERSION_PATTERN%" -txt
 
 rem  Take version of first found package.
@@ -114,9 +83,15 @@ for /f %%i in ('%get_version% "%dirs[1]%"') do (
   set version=%%i
 )
 
+if "%version%"=="" (
+  echo.
+  echo   [ERR] Can't read package version.
+  goto :END
+)
+
 echo OK
 
-echo|set/p=Check all found packages are of version %version% ... 
+echo|set/p=Checking whole packages group is of version %version% ... 
 
 rem  Compare next packages versions with first version.
 for /l %%i in (2,1,%dir_count%) do (
@@ -134,7 +109,7 @@ echo OK
 
 rem  -------------------------------------------------------------------------
 
-echo|set/p=Read packages URLs from info files ... 
+echo|set/p=Reading packages URLs from info files ... 
 
 rem  Each `_package.json` is expected to contain exacly one URL matching this
 set VSIX_URL_PATTERN=https:\/\/.*\.vsix
@@ -161,13 +136,13 @@ echo OK
 
 rem  --- Create batch downloader ---------------------------------------------
 
-echo|set/p=Writing packages downloader ... 
+echo|set/p=Writing downloader %group%.%version%.bat ... 
 
 set outfile=%root%\%group%.%version%.bat
 
 if exist "%outfile%" (
   echo.
-  echo   [WARN] Downloader %outfile% will be overwritten.
+  echo   [WARN] %outfile% will be overwritten.
   del "%outfile%"
 )
 
--- uncache/vc-x64-x64.bat
+++ uncache/vc-x64-x64.bat
@@ -1,14 +1,14 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-rem  Using:
-rem    vc-x64-x64 [VS cache path]
-
-rem  In root dir creates `vc-x64-x64.[VERSION].bat` downloader for %PACKAGES%
-rem  based on Visual Studio cache.
-
-setlocal
+rem  -------------------------------------------------------------------------
+rem  Using: `vc-x64-x64 [VS CACHE PATH]`.
+rem  In root dir creates `%GROUP%.[VERSION].bat` downloader for %PACKAGES%
+rem  based on its %path_patterns% in the Visual Studio cache.
 
 set GROUP=vc-x64-x64
+
+set PREFIX=Microsoft.VC.??.??.??.?.
 
 set PACKAGES=^
   CRT.Headers.base ^
@@ -19,9 +19,32 @@ set PACKAGES=^
   Tools.HostX64.TargetX64.base ^
   Tools.HostX64.TargetX64.Res.base
 
-set batch_path=%~dp0
+rem  -------------------------------------------------------------------------
+
+
 set cache_path=%~1
 
-call "%batch_path%\uncache" "%cache_path%" %GROUP% %PACKAGES%
+if "%cache_path%"=="" (
+  echo [ERR] Visual Studio cache path is not given.
+  goto :END
+)
 
+if not exist "%cache_path%" (
+  echo [ERR] Visual Studio cache path is not exists.
+  goto :END
+)
+
+
+set path_patterns=
+
+for %%i in (%PACKAGES%) do (
+  set path_patterns=!path_patterns! "%cache_path%\%PREFIX%%%i*"
+)
+
+
+set batch_dir=%~dp0
+call "%batch_dir%uncache" %GROUP% %path_patterns%
+
+
+:END
 endlocal
