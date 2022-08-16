151c2c46843f4f80b9f0864dc0badab2ff26eb00
reksarka <reksarka@gmail.com>
2022-08-16 11:23:51 +0300

    Automatically sets group from uncacher filename

--- uncache/msbuild.bat
+++ uncache/msbuild.bat
@@ -1,18 +1,2 @@
-@echo off
-setlocal
-
-
-rem  --------------------------------------------------------------------------
-rem  Using: `msbuild [VS CACHE PATH]`.
-rem  Creates `..\get\%GROUP%.[version].bat` downloader for %PACKAGES% using
-rem  info from MS Visual Studio %cache_path%.
-
-set GROUP=msbuild
-
-set PACKAGES=^
+@call "%~dp0uncache" %1 %~n0 ^
   "Microsoft.Build,version="
-rem  --------------------------------------------------------------------------
-
-
-call "%~dp0uncache" %1 %GROUP% %PACKAGES%
-endlocal
--- uncache/vc-x64-x64.bat
+++ uncache/vc-x64-x64.bat
@@ -1,15 +1,4 @@
-@echo off
-setlocal
-
-
-rem  --------------------------------------------------------------------------
-rem  Using: `vc-x64-x64 [VS CACHE PATH]`.
-rem  Creates `..\get\%GROUP%.[version].bat` downloader for %PACKAGES% using
-rem  info from MS Visual Studio %cache_path%.
-
-set GROUP=vc-x64-x64
-
-set PACKAGES=^
+@call "%~dp0uncache" %1 %~n0 ^
   "Microsoft.VC.??.??.??.?.CRT.Headers.base" ^
   "Microsoft.VC.??.??.??.?.CRT.Redist.X64.base" ^
   "Microsoft.VC.??.??.??.?.CRT.Source.base" ^
@@ -17,8 +6,3 @@ set PACKAGES=^
   "Microsoft.VC.??.??.??.?.CRT.x64.Store.base" ^
   "Microsoft.VC.??.??.??.?.Tools.HostX64.TargetX64.base" ^
   "Microsoft.VC.??.??.??.?.Tools.HostX64.TargetX64.Res.base"
-rem  -------------------------------------------------------------------------
-
-
-call "%~dp0uncache" %1 %GROUP% %PACKAGES%
-endlocal
